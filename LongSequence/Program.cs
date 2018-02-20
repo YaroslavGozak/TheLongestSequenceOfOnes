2006866927bd71a8a63a7c8e334a2b8f4b2c4888
Yaroslav Gozak <zcbr999@ukr.net>
2018-02-20 15:34:15 +0200

    Created an algorithm for finding the longest sequence of ones.
    - INPUT.TXT file is located in LongSequence/bin/Debug

---
+++ LongSequence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LongSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var sequence = ReadSequence("input.txt");
+                var length = GetTheLongestSequenceOfOnesLength(sequence);
+                Console.WriteLine(length);
+                Console.ReadKey();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error ocured while opening the file");
+            }
+        }
+
+        static string ReadSequence(string filePath)
+        {
+            var inputStream = new FileStream(filePath, FileMode.Open);
+            StreamReader reader = new StreamReader(inputStream);
+            return reader.ReadLine();
+        }
+
+        static int GetTheLongestSequenceOfOnesLength(string sequence)
+        {
+            var maxLength = 0;
+            var currrentLength = 0;
+            foreach(var digit in sequence)
+            {
+                if (String.Equals('1', digit))
+                {
+                    currrentLength++;
+                }
+                else if(String.Equals('0',digit))
+                {
+                    maxLength = Math.Max(currrentLength, maxLength);
+                    currrentLength = 0;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
